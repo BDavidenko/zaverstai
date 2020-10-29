64552f9f3dd3e6cfcccdf0cbeb5a2820518f3c67
Bogdan Davidenko <bdavidenko@yandex.ru>
2020-10-29 20:49:07 +0300

    feat: реализовать фильтр статей

---
+++ scripts/article.js
@@ -0,0 +1,58 @@
+// Кнопка лайк
+
+const likeButton = document.querySelector('.article__like');
+const likeNumber = document.querySelector('.article__like-counter');
+
+// Добавление коментария
+
+const commentForm = document.forms.comments; // нашли форму синтаксис только для форм
+const commentList = document.querySelector('.comments__list'); // нашли сам отец список куда будет вставлен коментарий
+const nameInput = commentForm.elements.name;  // нашли инпут для имени конкретной формы
+const textInput = commentForm.elements.text; // нашли инпут для текста конкретной формы
+const commentTemplate = document.querySelector('#comment-template').content; //нашли шаблон, синтаксис работает только с шаблонами
+const submitButton = commentForm.querySelector('.button');
+
+// переключение лайка
+
+function toggleLike() {
+  if (likeButton.classList.contains('added')) {
+    likeNumber.textContent--;
+  } else {
+    likeNumber.textContent++;
+  }
+
+  likeButton.classList.toggle('added');
+}
+
+// комментарии на странице статьи
+
+function addComment(evt) { // evt-event обьявляем функцию добавить коментарий.
+  evt.preventDefault(); // отменяем стандартное поведение, разрешаем изменение.
+
+  const comment = commentTemplate.querySelector('.comments__list-item').cloneNode(true); // клонируем саму ЛИ которая полетит в отца в виде коментария
+  const userName = comment.querySelector('.comments__name'); // нашли элемент в который будет вставляться текст с инпута имя
+  const userText = comment.querySelector('.comments__text'); // нашли элемент в который вставится текст коментария
+  userName.textContent = nameInput.value; // указали что текстовый контент будет братсья из инпута
+  userText.textContent = textInput.value;
+  commentForm.reset(); // обнулили форму после отправки коментария
+  commentList.append(comment); // указали отца в который добавляется новый элемент в низ содержимого
+}
+
+//контроль длинны комментария
+
+function checkValidation(input, submit) {
+  if (input.value.length < 9) {
+    input.classList.add('warning');
+    submit.disabled = true;
+  } else {
+    input.classList.remove('warning');
+    submit.disabled = false;
+  }
+}
+
+
+likeButton.addEventListener('click', toggleLike); // посадили слушателя на кнопку при клике указали функцию
+commentForm.addEventListener('submit', addComment); // посадили слушателя на форму при отправке
+textInput.addEventListener('keydown', () => {
+  checkValidation(textInput, submitButton);
+});
--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,20 @@
-// Кнопка лайк
-const likeButton = document.querySelector('.article__like');
-const likeNumber = document.querySelector('.article__like-counter');
-// Добавление коментария
-const commentForm = document.forms.comments; // нашли форму синтаксис только для форм
-const commentList = document.querySelector('.comments__list'); // нашли сам отец список куда будет вставлен коментарий
-const nameInput = commentForm.elements.name;  // нашли инпут для имени конкретной формы
-const textInput = commentForm.elements.text; // нашли инпут для текста конкретной формы
-const commentTemplate = document.querySelector('#comment-template').content; //нашли шаблон, синтаксис работает только с шаблонами
-// выпадающее меню
-// переключение лайка
-function toggleLike() {
-  if (likeButton.classList.contains('added')) {
-    likeNumber.textContent--;
-  } else {
-    likeNumber.textContent++;
-  }
+const dropdownList = document.querySelector('.articles__dropdown-list');
+const tagList = document.querySelectorAll('.tag');
 
-  likeButton.classList.toggle('added');
-}
-// комментарии на странице статьи
-function addComment(evt) { // evt-event обьявляем функцию добавить коментарий.
-  evt.preventDefault(); // отменяем стандартное поведение, разрешаем изменение.
+const numbers = [4, 8, 15, 16, 23, 42];
 
-  const comment = commentTemplate.querySelector('.comments__list-item').cloneNode(true); // клонируем саму ЛИ которая полетит в отца в виде коментария
-  const userName = comment.querySelector('.comments__name'); // нашли элемент в который будет вставляться текст с инпута имя
-  const userText = comment.querySelector('.comments__text'); // нашли элемент в который вставится текст коментария
-  userName.textContent = nameInput.value; // указали что текстовый контент будет братсья из инпута
-  userText.textContent = textInput.value;
-  commentForm.reset(); // обнулили форму после отправки коментария
-  commentList.append(comment); // указали отца в который добавляется новый элемент в низ содержимого
-}
+console.log(typeof(tagList));
+
+tagList[0];
+
+
 // фильтр по тегам
 function filterTag(evt) {
-  const articleTheme = evt.target;
+  const articleTheme = evt.target.textContent;
+
+
+
   console.log(articleTheme);
 }
 
-
-likeButton.addEventListener('click', toggleLike); // посадили слушателя на кнопку при клике указали функцию
-commentForm.addEventListener('submit', addComment); // посадили слушателя на форму при отправке
-document.addEventListener('click', (evt) => {
-  if {
-
-  }
-})
+dropdownList.addEventListener('click', filterTag);
