fa4b92d0e09c805c2ff7b7a428b2592c161aeb0f
Bogdan Davidenko <bdavidenko@yandex.ru>
2020-10-29 13:54:55 +0300

    feat: реализовать фильтр статей

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,13 @@
 const likeButton = document.querySelector('.article__like');
 const likeNumber = document.querySelector('.article__like-counter');
 // Добавление коментария
-const commentForm = document.forms.comments; //нашли форму синтаксис только для форм
-const commentList = document.querySelector('.comments__list'); //нашли сам отец список куда будет вставлен коментарий
-const nameInput = commentForm.elements.name;  //нашли инпут для имени конкретной формы
-const textInput = commentForm.elements.text; //нашли инпут для текста конкретной формы
+const commentForm = document.forms.comments; // нашли форму синтаксис только для форм
+const commentList = document.querySelector('.comments__list'); // нашли сам отец список куда будет вставлен коментарий
+const nameInput = commentForm.elements.name;  // нашли инпут для имени конкретной формы
+const textInput = commentForm.elements.text; // нашли инпут для текста конкретной формы
 const commentTemplate = document.querySelector('#comment-template').content; //нашли шаблон, синтаксис работает только с шаблонами
-
+// выпадающее меню
+// переключение лайка
 function toggleLike() {
   if (likeButton.classList.contains('added')) {
     likeNumber.textContent--;
@@ -17,19 +18,29 @@ function toggleLike() {
 
   likeButton.classList.toggle('added');
 }
-
-function addComment(evt) { //evt-event обьявляем функцию добавить коментарий.
+// комментарии на странице статьи
+function addComment(evt) { // evt-event обьявляем функцию добавить коментарий.
   evt.preventDefault(); // отменяем стандартное поведение, разрешаем изменение.
 
-  const comment = commentTemplate.querySelector('.comments__list-item').cloneNode(true); //клонируем саму ЛИ которая полетит в отца в виде коментария
+  const comment = commentTemplate.querySelector('.comments__list-item').cloneNode(true); // клонируем саму ЛИ которая полетит в отца в виде коментария
   const userName = comment.querySelector('.comments__name'); // нашли элемент в который будет вставляться текст с инпута имя
   const userText = comment.querySelector('.comments__text'); // нашли элемент в который вставится текст коментария
-  userName.textContent = nameInput.value; //указали что текстовый контент будет братсья из инпута
+  userName.textContent = nameInput.value; // указали что текстовый контент будет братсья из инпута
   userText.textContent = textInput.value;
-  commentForm.reset(); //обнулили форму после отправки коментария
-  commentList.append(comment); //указали отца в который добавляется новый элемент в низ содержимого
+  commentForm.reset(); // обнулили форму после отправки коментария
+  commentList.append(comment); // указали отца в который добавляется новый элемент в низ содержимого
+}
+// фильтр по тегам
+function filterTag(evt) {
+  const articleTheme = evt.target;
+  console.log(articleTheme);
 }
 
 
-likeButton.addEventListener('click', toggleLike); //посадили слушателя на кнопку при клике указали функцию
-commentForm.addEventListener('submit', addComment); //посадили слушателя на форму при отправке
+likeButton.addEventListener('click', toggleLike); // посадили слушателя на кнопку при клике указали функцию
+commentForm.addEventListener('submit', addComment); // посадили слушателя на форму при отправке
+document.addEventListener('click', (evt) => {
+  if {
+
+  }
+})
